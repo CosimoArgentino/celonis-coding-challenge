8fe8a65dcbde5ae5db1378d739e402a58594ba6d
Cosimo Argentino <c.argentino@radicalstorage.com>
2023-06-29 18:25:28 +0200

    service updated

--- src/main/java/com/celonis/weather/service/WeatherService.java
+++ src/main/java/com/celonis/weather/service/WeatherService.java
@@ -1,15 +1,18 @@
 package com.celonis.weather.service;
 
 import com.celonis.weather.model.Weather;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+
 @Service
 public class WeatherService implements WeatherServiceInterface{
-
+    private final static Logger logger = LoggerFactory.getLogger(WeatherService.class);
     @Value("${apiKey}")
     private String apiKey;
     @Value("${weatherApi}")
@@ -27,7 +30,9 @@ public class WeatherService implements WeatherServiceInterface{
                     .queryParam("key", apiKey);
 
             ResponseEntity<Weather> response = restTemplate.getForEntity(builder.toUriString(), Weather.class);
+            //TODO use elasticsearch (...) or maybe just a static map to create an in-memory database/cache
         }catch(Exception exc){
+            logger.error(String.format("error on fetching weather for %s, error message: %s", city, exc.getMessage()));
             throw exc;
         }
     }
