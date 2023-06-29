82a685790f4e93760ee5cb4de6b0fc19b1e8401b
Cosimo Argentino <c.argentino@radicalstorage.com>
2023-06-29 15:00:29 +0200

    First commit

---
+++ src/test/java/com/celonis/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celonis.weather;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
