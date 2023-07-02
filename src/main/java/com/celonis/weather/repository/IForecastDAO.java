ab7aeb0864ecf24823c197ab8aeb3c861060de7f
Cosimo Argentino <c.argentino@radicalstorage.com>
2023-07-02 11:37:52 +0200

    useless repository annotation removed

--- src/main/java/com/celonis/weather/repository/IForecastDAO.java
+++ src/main/java/com/celonis/weather/repository/IForecastDAO.java
@@ -2,11 +2,9 @@ package com.celonis.weather.repository;
 
 import com.celonis.weather.model.forecast.ForecastEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
 
-@Repository
 public interface IForecastDAO extends JpaRepository<ForecastEntity, Long> {
     ForecastEntity findByNameAndDate(String name, Date date);
 }
