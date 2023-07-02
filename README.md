5e7f2f8e8c2356d01a71effbe44aaa3bafca8afe
Cosimo Argentino <c.argentino@radicalstorage.com>
2023-07-02 14:54:15 +0200

    invalidate cache method added

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # celonis-coding-challenge
-Celonis coding challenge
+
+
+Welcome to the weather api challenge! <br>
+Hoping you'll enjoy the code, there'e a few step to take in order to run the application.
+
+
+## Installation <br>
+
+Just a few step:
+
+- Install the latest LTS Java version (Java 17) and maven
+- Update the application.properties and the application-test.properties inside the resources folder for main and test and put your api key for the weather api: 
+- ```bash 
+  apiKey=<your api key>
+  ```
+- run the follow command to install all dependencies and run tests
+- ```bash
+  mvn clean install
+  ```
+- have fun!
--- src/test/java/com/celonis/weather/ForecastApiIntegrationTest.java
+++ src/test/java/com/celonis/weather/ForecastApiIntegrationTest.java
@@ -85,10 +85,70 @@ public class ForecastApiIntegrationTest {
     }
 
     @Test
-    public void testFetch_NotSaveCity_status404() throws Exception {
+    public void testFetch_NotSavedCity_status404() throws Exception {
         mvc.perform(get("/weather/forecast/fetch/Madrid")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testFetch_ExistingCityOneDay_status200() throws Exception {
+        String cityName = "Madrid";
+
+        mvc.perform(post(String.format("/weather/forecast/save/%s", cityName))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+        LocalDate tomorrow = LocalDate.now().plusDays(1);
+
+        mvc.perform(get(String.format("/weather/forecast/fetch/Madrid?date=%s", tomorrow.toString()))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$[0].name", Matchers.is(cityName)))
+                .andExpect(jsonPath("$[0].date", Matchers.is(tomorrow.toString())));
+
+        long deleted = forecastDAO.deleteByName(cityName);
+        Assert.assertEquals(2, deleted);
+        cache.invalidateAll();
+    }
+
+    @Test
+    public void testFetch_SaveTwoCityAndFetchAll_status200() throws Exception {
+        String cityName1 = "Madrid";
+        String cityName2 = "Taranto";
+
+        mvc.perform(post(String.format("/weather/forecast/save/%s", cityName1))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+        mvc.perform(post(String.format("/weather/forecast/save/%s", cityName2))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+        LocalDate now = LocalDate.now();
+        LocalDate tomorrow = now.plusDays(1);
+
+        mvc.perform(get(String.format("/weather/forecast/fetch"))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", Matchers.hasSize(4)))
+                .andExpect(jsonPath("$[0].name", Matchers.is(cityName1)))
+                .andExpect(jsonPath("$[0].date", Matchers.is(now.toString())))
+                .andExpect(jsonPath("$[1].name", Matchers.is(cityName1)))
+                .andExpect(jsonPath("$[1].date", Matchers.is(tomorrow.toString())))
+                .andExpect(jsonPath("$[2].name", Matchers.is(cityName2)))
+                .andExpect(jsonPath("$[2].date", Matchers.is(now.toString())))
+                .andExpect(jsonPath("$[3].name", Matchers.is(cityName2)))
+                .andExpect(jsonPath("$[3].date", Matchers.is(tomorrow.toString())));
+
+        long deleted = forecastDAO.deleteByName(cityName1);
+        Assert.assertEquals(2, deleted);
+        deleted = forecastDAO.deleteByName(cityName2);
+        Assert.assertEquals(2, deleted);
+        cache.invalidateAll();
     }
 
 }
